de081a8946bfe520f5803334b755092dddcda561
contatophcunha <47146761+contatophcunha@users.noreply.github.com>
2021-06-07 21:46:37 +0100

    Add files via upload

---
+++ calcflex.js
@@ -0,0 +1,20 @@
+
+
+var etanol,gasolina;
+
+function calcular(){
+    etanol=parseFloat(frmFlex.txtEtanol.value.replace(",","."));
+    gasolina=parseFloat(frmFlex.txtGasolina.value.replace(",","."));
+
+    if(etanol<0.7*gasolina){
+        document.getElementById("status").src="images/etanol.png";
+    }else{
+        document.getElementById("status").src="images/gasolina.png";
+
+    }
+}
+
+function resetar(){
+    document.getElementById("status").src="images/neutro.png"
+
+}
